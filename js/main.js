3b81b34a4d089ff831ff77a0bcaffc612a31257b
AnarchyBurger <n.l.schneider@gmail.com>
2018-08-10 12:18:39 -0400

    Adjusted formatting

--- js/main.js
+++ js/main.js
@@ -2,9 +2,24 @@ var app = (function () {
 
     var pictureFrame = document.getElementById('pictureFrame');
     var picture = document.createElement('img');
+    var randomNumber = 0,
+        thisRandomNumber = 1;
 
-    picture.src = "img/1.png";
-    picture.classList = "picture";
-    pictureFrame.appendChild(picture);
+    addPicture();
 
+    var loopTimer = setInterval(function () {
+        picture.remove();
+        addPicture();
+    }, 5000);
+
+    function addPicture() {
+        picture.src = "img/" + makeRandomNumber() + ".png";
+        picture.classList = "picture";
+        pictureFrame.appendChild(picture);
+    }
+
+    function makeRandomNumber() {
+        randomNumber = Math.floor(Math.random() * 10 + 1);
+        return randomNumber;
+    }
 })()
